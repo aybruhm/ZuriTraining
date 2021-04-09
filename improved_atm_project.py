775edd9f1e6767ed3ed49d692085528352f67274
Israel Abraham <israelvictory87@gmail.com>
2021-04-09 15:41:03 +0100

    Created generate_acc_number and register logic

--- improved_atm_project.py
+++ improved_atm_project.py
@@ -7,23 +7,83 @@ Improve on your ATM mockup from last course to include the following:
 4. Any other improvement you can think of (extra point)
 """
 
+import random
+import time
+
+database = {}
+
+
 def init():
-    pass
+    print("\nWelcome to IsraelBANK")
+    print("Do you have an account?")
+    option = str.capitalize(input("Select Y - yes or N - no: "))
+
+    if (option == "Y"):
+        login()
+    elif (option == "N"):
+        print(register())
+    else:
+        print("Oops. You selected an invalid option")
+        print("Do you wish to continue?")
+        continue_option = str.capitalize(input("Select Y - yes or N - no: "))
+
 
 def register():
-    pass
+    print("\n############ Register ############")
+
+    email = input("Your Email Address: ")
+    first_name = input("Your First Name: ")
+    last_name = input("Your Last Name: ")
+    password = int(input("Your Password: "))
+    account_number = generate_acc_number()
+    database[account_number] = [first_name, last_name, email, password]
+
+    print("Your account has been created!")
+    print(f"Your account number is: {account_number}")
+
+    option = str.capitalize(
+        input("\nDo you want to login now? Y - yes and N - no: ")
+    )
+    if (option == "Y"):
+        login()
+    elif (option == "N"):
+        print("Thank you for banking with us.")
+    else:
+        print("Oof. You have selected an invalid option.")
+
+    return database
+
 
 def login():
-    pass
+    print("\n############ Login ############")
+    email = input("Your Email Address: ")
+    password = int(input("Your Password: "))
+
+    for email, password in database.items():
+        print(email, password)
+
 
 def generate_acc_number():
+    print("\nGenerating Account Number...")
+    time.sleep(2)
+    print("Account Number Generated!")
+    return random.randrange(000000000000, 11111111111)
+
+
+def bankingOperation():
     pass
+
 
 def withdrawal():
     pass
 
+
 def deposit():
     pass
 
+
 def complaint():
     pass
+
+
+init()
